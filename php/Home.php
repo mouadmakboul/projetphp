840b2f5580e3383679c30221ad055a31f77a3890
Mouad Makboul <mouadmakboul8@gmail.com>
2023-05-07 16:18:42 +0100

    i finish for today

--- php/Home.php
+++ php/Home.php
@@ -1,26 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<?php
+session_start();
+if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {
 
 
-</head>
+?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <div class="d-flex justify-content-center align-items-center vh-100">
 
-        
-            <h4 class="text-center">Welcome mouad to ur application</h4><br>
+    </head>
 
-        
-    </div>
-</body>
+    <body>
 
-</html>
+        <div class="d-flex justify-content-center align-items-center vh-100">
+
+
+            <h4 class="text-center display-4">Welcome mouad to ur application <?= $_SESSION['fname'] ?></h4>
+            <br>
+            <a href="logout.php" class="btn btn-warning vh-300">Logout</a>
+
+            
+        </div>
+    </body>
+
+    </html>
+<?php } else {
+    header("location:login.php");
+}
